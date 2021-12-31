0990fd00ff279707e2ae23faf67912a325932e0b
Paul Thebaud <paul.thebaud29@gmail.com>
2021-12-31 16:07:41 +0100

    Add amount input to transfer screen

--- src/screens/TransferScreen.js
+++ src/screens/TransferScreen.js
@@ -1,10 +1,40 @@
+import AmountInput from '@/components/inputs/AmountInput';
+import { useEffect, useState } from 'react';
 import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { ActivityIndicator, Text } from 'react-native-paper';
+
+async function asyncFunctionToGetMin() {
+  return 2.5;
+}
 
 export default function TransferScreen() {
+  const [loading, setLoading] = useState(false);
+  const [minAmount, setMinAmount] = useState(undefined);
+
+  // TODO Remove this. This is just an example on how to retrieve an async content.
+  useEffect(() => {
+    async function fetchMin() {
+      setMinAmount(await asyncFunctionToGetMin());
+      setLoading(false);
+    }
+
+    fetchMin();
+  }, []);
+
   return (
-    <View>
-      <Text>Transfer!</Text>
+    <View style={{ padding: 8 }}>
+      {loading ? (
+        <ActivityIndicator animating />
+      ) : (
+        <>
+          <Text>Transfer!</Text>
+          <AmountInput
+            min={minAmount}
+            hint={`Amount to transfer (minimum of ${minAmount} CSPR)`}
+            funds="1000"
+          />
+        </>
+      )}
     </View>
   );
 }
