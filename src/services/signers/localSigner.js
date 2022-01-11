9a627a02a43a4c1eeebc59e3d85f68fbb31957e4
Paul Thebaud <paul.thebaud29@gmail.com>
2022-01-12 00:54:23 +0100

    Add key computation cache

--- src/services/signers/localSigner.js
+++ src/services/signers/localSigner.js
@@ -1,11 +1,21 @@
 import generateAsymmetricKey from '@/services/signers/generateAsymmetricKey';
 import { LocalSigner } from '@casperholders/core/dist/services/signers/localSigner';
 
+const cachedKeys = {};
+
+function generateAsymmetricKeyAndCache(key) {
+  if (!(key in cachedKeys)) {
+    cachedKeys[key] = generateAsymmetricKey(key);
+  }
+
+  return cachedKeys[key];
+}
+
 export default {
   id: 'LOCAL_SIGNER',
   signer: LocalSigner,
-  getPublicKey: (state) => generateAsymmetricKey(state.auth.activeKey).publicKey.toHex(),
+  getPublicKey: (state) => generateAsymmetricKeyAndCache(state.auth.activeKey).publicKey.toHex(),
   getOptionsForTransfer: (state) => ({
-    key: generateAsymmetricKey(state.auth.activeKey),
+    key: generateAsymmetricKeyAndCache(state.auth.activeKey),
   }),
 };
