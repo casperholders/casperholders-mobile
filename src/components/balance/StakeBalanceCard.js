b6e523916cbbf8e34a393f4b734a4c8a2019ea0a
Killian Hascoet <killianh@live.fr>
2022-01-11 05:06:06 +0100

    Add stake buttons

--- src/components/balance/StakeBalanceCard.js
+++ src/components/balance/StakeBalanceCard.js
@@ -1,10 +1,16 @@
 import formatCasperAmount from '@/helpers/formatCasperAmount';
 import truncateInMiddle from '@/helpers/truncateInMiddle';
-import { Caption, Card, Paragraph } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
+import { Button, Caption, Card, Paragraph } from 'react-native-paper';
 
 export default function ({ validator, staked, formattedPercentOfTotal, delegationRate }) {
   const description = `${formattedPercentOfTotal}% of your staked funds ꞏ Validator ${truncateInMiddle(validator)}`;
-
+  const styles = StyleSheet.create({
+    actions: {
+      alignItems: 'flex-end',
+      justifyContent: 'flex-end',
+    },
+  });
   return (
     <Card style={{ marginLeft: 12, marginBottom: 12 }}>
       <Card.Content>
@@ -14,6 +20,13 @@ export default function ({ validator, staked, formattedPercentOfTotal, delegatio
         <Caption>
           {description}
         </Caption>
+        <Card.Actions style={styles.actions}>
+          <Button
+            mode="contained"
+            style={{ marginRight: 8 }}
+          >Stake</Button>
+          <Button mode="contained">Unstake</Button>
+        </Card.Actions>
       </Card.Content>
     </Card>
   );
