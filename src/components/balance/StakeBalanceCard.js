2121980742639afe89675a750c344e6e5191fa54
Paul Thebaud <paul.thebaud29@gmail.com>
2022-02-02 22:07:08 +0100

    Use card with icons on stake balance card

--- src/components/balance/StakeBalanceCard.js
+++ src/components/balance/StakeBalanceCard.js
@@ -1,10 +1,11 @@
+import CardWithIcons from '@/components/common/CardWithIcons';
 import ValidatorIcon from '@/components/common/ValidatorIcon';
 import formatCasperAmount from '@/helpers/formatCasperAmount';
 import truncateInMiddle from '@/helpers/truncateInMiddle';
 import useAdapter from '@/hooks/auth/useAdapter';
 import ReadOnlyAdapter from '@/services/signers/readOnlyAdapter';
 import { StyleSheet, View } from 'react-native';
-import { Button, Caption, Card, Paragraph, useTheme } from 'react-native-paper';
+import { Button, Caption, Paragraph, useTheme } from 'react-native-paper';
 
 /**
  * Stake balance card component
@@ -19,59 +20,58 @@ import { Button, Caption, Card, Paragraph, useTheme } from 'react-native-paper';
 export default function (
   { navigation, validator, staked, image, formattedPercentOfTotal, delegationRate },
 ) {
+  const theme = useTheme();
   const adapter = useAdapter();
   const readOnly = adapter.constructor.ID === ReadOnlyAdapter.ID;
-  const theme = useTheme();
   const description = `${formattedPercentOfTotal}% of your staked funds - ${delegationRate}% fee`;
 
   return (
-    <Card style={styles.card}>
-      <Card.Content style={{ flexDirection: 'row' }}>
-        {
-          image !== undefined && <ValidatorIcon
-            url={image}
-            size={35}
-            left
-          />
-        }
-        <View style={{ flex: 1 }}>
-          <Paragraph>
-            Validator {truncateInMiddle(validator)}
-          </Paragraph>
-          <Paragraph>
-            {formatCasperAmount(staked)}
-          </Paragraph>
-          <Caption>
-            {description}
-          </Caption>
-        </View>
-      </Card.Content>
-      <Card.Actions style={styles.actions}>
+    <CardWithIcons
+      style={styles.card}
+      left={image !== undefined && <ValidatorIcon
+        style={styles.cardIcon}
+        url={image}
+        size={35}
+        left
+      />}
+    >
+      <View>
+        <Paragraph>
+          Validator {truncateInMiddle(validator)}
+        </Paragraph>
+        <Paragraph>
+          {formatCasperAmount(staked)}
+        </Paragraph>
+        <Caption>
+          {description}
+        </Caption>
+      </View>
+      <View style={styles.cardActions}>
         <Button
-          style={styles.actionsStake}
+          style={styles.cardActionsStake}
           color={theme.colors.text}
+          disabled={readOnly}
           onPress={() => navigation.jumpTo('OperationsTab', {
             screen: 'Stake',
             address: validator,
             initial: false,
           })}
-          disabled={readOnly}
         >
           Stake
         </Button>
         <Button
           color={theme.colors.text}
+          disabled={readOnly}
           onPress={() => navigation.jumpTo('OperationsTab', {
             screen: 'Unstake',
             address: validator,
             initial: false,
           })}
-          disabled={readOnly}
         >
           Unstake
         </Button>
-      </Card.Actions>
-    </Card>
+      </View>
+    </CardWithIcons>
   );
 }
 
@@ -80,11 +80,15 @@ const styles = StyleSheet.create({
     marginLeft: 12,
     marginBottom: 12,
   },
-  actions: {
+  cardIcon: {
+    marginBottom: 'auto',
+  },
+  cardActions: {
+    flexDirection: 'row',
     alignItems: 'flex-end',
     justifyContent: 'flex-end',
   },
-  actionsStake: {
+  cardActionsStake: {
     marginRight: 8,
   },
 });
