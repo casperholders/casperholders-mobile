f81e7ea38f6631d9cc40e21d78cc025a9201242a
Killian Hascoet <killianh@live.fr>
2022-02-17 19:36:15 +0100

    Set correct auction contracts

--- src/services/networks/testnetAdapter.js
+++ src/services/networks/testnetAdapter.js
@@ -33,6 +33,6 @@ export default class TestnetAdapter extends AbstractNetworkAdapter {
   }
 
   get auctionManagerHash() {
-    return 'ccb576d6ce6dec84a551e48f0d0b7af89ddba44c7390b690036257a04a3ae9ea';
+    return '93d923e336b20a4c4ca14d592b60e5bd3fe330775618290104f9beb326db7ae2';
   }
 }
