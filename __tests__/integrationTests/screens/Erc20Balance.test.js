bb6ed600998376c1e527be1a5dda68fea0a4b4f9
Paul Thébaud <paul.thebaud29@gmail.com>
2023-01-11 00:45:00 +0100

    test: erc20 account with tokens

--- __tests__/integrationTests/screens/Erc20Balance.test.js
+++ __tests__/integrationTests/screens/Erc20Balance.test.js
@@ -1,6 +1,7 @@
 import AppProvider from '@/AppProvider';
 import Main from '@/Main';
 import MainnetAdapter from '@/services/networks/mainnetAdapter';
+import TestnetAdapter from '@/services/networks/testnetAdapter';
 import LocalAdapter from '@/services/signers/localAdapter';
 import { getStore, resetStore } from '@/store';
 import { connect } from '@/store/reducers/authReducer';
@@ -15,7 +16,7 @@ beforeEach(() => {
 jest.setTimeout(500000);
 
 describe('Erc20Balances.js', () => {
-  test('Should show balance without tokens', async () => {
+  test('Should show ERC20 balance without tokens', async () => {
     getStore().dispatch(
       connect({
         adapterId: LocalAdapter.ID,
@@ -38,5 +39,37 @@ describe('Erc20Balances.js', () => {
     await waitFor(() => {
       expect(queryByText('0 tokens with funds')).toBeTruthy();
     }, { timeout: 10000 });
+  });
+
+  test('Should show ERC20 balance with tokens', async () => {
+    getStore().dispatch(
+      connect({
+        adapterId: LocalAdapter.ID,
+        options: { privateKey: TEST_LOCAL_SIGNER_KEY },
+      }),
+    );
+
+    getStore().dispatch(
+      setNetwork({
+        network: TestnetAdapter.ID,
+      }),
+    );
+
+    const { getByTestId, queryByText } = render(<Main initialRoute="BalanceTab" />, {
+      wrapper: AppProvider,
+    });
+
+    expect(queryByText('ERC20 tokens')).toBeTruthy();
+
+    await waitFor(() => {
+      expect(queryByText('7 tokens with funds')).toBeTruthy();
+    }, { timeout: 10000 });
+
+    const erc20ListToggle = getByTestId('erc20Toggle');
+
+    erc20ListToggle.press();
+
+    expect(queryByText('Wrapped Casper')).toBeTruthy();
+    expect(queryByText(/\d+\.\d{5} WCSPR/)).toBeTruthy();
   });
 });
